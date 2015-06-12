KOBUGE-Incubator/ringed

commit 3028e1eb6f7123ef2699089df5ff6ab6f45d0bb6

++Adding Lights to the game, little issue with background

scenes/player.xml
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<resource_file type="PackedScene" subresource_count="10" version="1.1" version_name="Godot Engine v1.1.rc3.custom_build">
+<resource_file type="PackedScene" subresource_count="11" version="1.1" version_name="Godot Engine v1.1.stable.d3b42e41b0">
 	<ext_resource path="res://scripts/player.gd" type="Script"></ext_resource>
+	<ext_resource path="res://gfx/light.png" type="Texture"></ext_resource>
 	<ext_resource path="res://scenes/gun.xml" type="PackedScene"></ext_resource>
 	<ext_resource path="res://scenes/gun_bomb.xml" type="PackedScene"></ext_resource>
 	<ext_resource path="res://gfx/player.png" type="Texture"></ext_resource>
@@ -79,7 +80,7 @@
 			<string> "conns" </string>
 			<int_array  len="0"> 				 </int_array>
 			<string> "names" </string>
-			<string_array  len="93">
+			<string_array  len="108">
 				<string> "player" </string>
 				<string> "RigidBody2D" </string>
 				<string> "_import_path" </string>
@@ -126,6 +127,23 @@
 				<string> "hide_when_full" </string>
 				<string> "shot_cooldown" </string>
 				<string> "gun_change_cooldown" </string>
+				<string> "Light2D" </string>
+				<string> "enabled" </string>
+				<string> "texture" </string>
+				<string> "offset" </string>
+				<string> "scale" </string>
+				<string> "color" </string>
+				<string> "energy" </string>
+				<string> "range/height" </string>
+				<string> "range/z_min" </string>
+				<string> "range/z_max" </string>
+				<string> "range/layer_min" </string>
+				<string> "range/layer_max" </string>
+				<string> "range/item_mask" </string>
+				<string> "shadow/enabled" </string>
+				<string> "shadow/buffer_size" </string>
+				<string> "shadow/esm_multiplier" </string>
+				<string> "shadow/item_mask" </string>
 				<string> "CollisionShape2D" </string>
 				<string> "shape" </string>
 				<string> "trigger" </string>
@@ -135,9 +153,7 @@
 				<string> "Normal" </string>
 				<string> "Bomb" </string>
 				<string> "Sprite" </string>
-				<string> "texture" </string>
 				<string> "centered" </string>
-				<string> "offset" </string>
 				<string> "flip_h" </string>
 				<string> "flip_v" </string>
 				<string> "vframes" </string>
@@ -175,11 +191,11 @@
 				<string> "autoplay" </string>
 			</string_array>
 			<string> "node_count" </string>
-			<int> 12 </int>
+			<int> 13 </int>
 			<string> "nodes" </string>
-			<int_array  len="464"> 				-1, -1, 1, 0, -1, 44, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 14, 10, 15, 11, 16, 8, 17, 12, 18, 13, 19, 8, 20, 3, 21, 3, 22, 9, 23, 2, 24, 2, 25, 5, 26, 2, 27, 8, 28, 7, 29, 7, 30, 8, 31, 8, 32, 1, 33, 4, 34, 5, 35, 14, 36, 15, 37, 16, 38, 17, 39, 18, 40, 19, 41, 2, 42, 20, 43, 8, 44, 21, 45, 22, 0, 0, 0, 46, 46, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 6, 10, 7, 11, 1, 47, 10, 48, 8, 0, 0, 0, 46, 49, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 24, 8, 5, 9, 6, 10, 7, 11, 1, 47, 12, 48, 8, 0, 0, 0, 51, 50, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 25, 8, 5, 9, 6, 10, 7, 11, 1, 0, 3, 0, 51, 52, 26, 2, 2, 0, 38, 27, 0, 3, 0, 51, 53, 28, 3, 2, 0, 3, 8, 38, 29, 0, 0, 0, 54, 54, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 30, 8, 5, 9, 6, 10, 7, 11, 1, 55, 31, 56, 1, 57, 4, 58, 8, 59, 8, 60, 3, 61, 3, 62, 7, 63, 32, 64, 8, 65, 33, 0, 0, 0, 66, 66, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 56, 1, 67, 8, 68, 1, 69, 34, 70, 6, 71, 35, 72, 35, 73, 36, 74, 36, 75, 1, 76, 1, 77, 37, 78, 37, 79, 37, 80, 37, 0, 0, 0, 51, 81, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 9, 11, 1, 0, 8, 0, 54, 82, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 38, 8, 5, 9, 6, 10, 7, 11, 1, 55, 39, 56, 8, 57, 4, 58, 8, 59, 8, 60, 3, 61, 3, 62, 7, 63, 32, 64, 1, 65, 40, 0, 8, 0, 54, 83, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 38, 8, 5, 9, 6, 10, 7, 11, 1, 55, 41, 56, 8, 57, 4, 58, 8, 59, 8, 60, 3, 61, 3, 62, 7, 63, 32, 64, 1, 65, 40, 0, 0, 0, 84, 84, -1, 9, 2, 0, 85, 3, 86, 5, 87, 42, 88, 43, 89, 1, 90, 2, 91, 44, 92, 45, 0 </int_array>
+			<int_array  len="525"> 				-1, -1, 1, 0, -1, 44, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 14, 10, 15, 11, 16, 8, 17, 12, 18, 13, 19, 8, 20, 3, 21, 3, 22, 9, 23, 2, 24, 2, 25, 5, 26, 2, 27, 8, 28, 7, 29, 7, 30, 8, 31, 8, 32, 1, 33, 4, 34, 5, 35, 14, 36, 15, 37, 16, 38, 17, 39, 18, 40, 19, 41, 2, 42, 20, 43, 8, 44, 21, 45, 22, 0, 0, 0, 46, 46, -1, 27, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 24, 10, 7, 11, 1, 47, 1, 48, 25, 49, 4, 50, 2, 51, 26, 52, 2, 22, 7, 53, 2, 54, 27, 55, 28, 56, 7, 57, 7, 58, 3, 59, 1, 60, 29, 61, 30, 62, 3, 0, 0, 0, 63, 63, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 31, 8, 5, 9, 6, 10, 7, 11, 1, 64, 10, 65, 8, 0, 0, 0, 63, 66, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 32, 8, 5, 9, 6, 10, 7, 11, 1, 64, 12, 65, 8, 0, 0, 0, 68, 67, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 33, 8, 5, 9, 6, 10, 7, 11, 1, 0, 4, 0, 68, 69, 34, 2, 2, 0, 38, 35, 0, 4, 0, 68, 70, 36, 3, 2, 0, 3, 8, 38, 37, 0, 0, 0, 71, 71, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 38, 8, 5, 9, 6, 10, 7, 11, 1, 48, 39, 72, 1, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 8, 80, 41, 0, 0, 0, 81, 81, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 72, 1, 82, 8, 83, 1, 84, 42, 85, 6, 86, 43, 87, 43, 88, 44, 89, 44, 90, 1, 91, 1, 92, 45, 93, 45, 94, 45, 95, 45, 0, 0, 0, 68, 96, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 9, 11, 1, 0, 9, 0, 71, 97, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 46, 8, 5, 9, 6, 10, 7, 11, 1, 48, 47, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 1, 80, 48, 0, 9, 0, 71, 98, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 46, 8, 5, 9, 6, 10, 7, 11, 1, 48, 49, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 1, 80, 48, 0, 0, 0, 99, 99, -1, 9, 2, 0, 100, 3, 101, 5, 102, 50, 103, 51, 104, 1, 105, 2, 106, 52, 107, 53, 0 </int_array>
 			<string> "variants" </string>
-			<array  len="46" shared="false">
+			<array  len="54" shared="false">
 				<node_path> "" </node_path>
 				<bool> True </bool>
 				<real> 1 </real>
@@ -199,13 +215,13 @@
 				<resource  resource_type="Script" path="res://scripts/player.gd">  </resource>
 				<dictionary  shared="false">
 					<string> "__editor_plugin_screen__" </string>
-					<string> "Script" </string>
+					<string> "2D" </string>
 					<string> "__editor_plugin_states__" </string>
 					<dictionary  shared="false">
 						<string> "2D" </string>
 						<dictionary  shared="false">
 							<string> "ofs" </string>
-							<vector2> -223.16, -132.093 </vector2>
+							<vector2> -363.398, -201.034 </vector2>
 							<string> "snap_grid" </string>
 							<bool> False </bool>
 							<string> "snap_offset" </string>
@@ -225,7 +241,7 @@
 							<string> "snap_step" </string>
 							<vector2> 10, 10 </vector2>
 							<string> "zoom" </string>
-							<real> 3.794779 </real>
+							<real> 1.50734 </real>
 						</dictionary>
 						<string> "3D" </string>
 						<dictionary  shared="false">
@@ -322,7 +338,7 @@
 						<string> "Script" </string>
 						<dictionary  shared="false">
 							<string> "current" </string>
-							<int> 5 </int>
+							<int> 0 </int>
 							<string> "sources" </string>
 							<array  len="7" shared="false">
 								<string> "res://scripts/moveable_object.gd" </string>
@@ -348,6 +364,14 @@
 				<real> 10 </real>
 				<real> 0.1 </real>
 				<real> 0.2 </real>
+				<vector2> 5.29622, 0.776039 </vector2>
+				<vector2> 0.595073, 0.595073 </vector2>
+				<resource  resource_type="Texture" path="res://gfx/light.png">  </resource>
+				<color> 0.681111, 0.681111, 0.681111, 1 </color>
+				<int> -1024 </int>
+				<int> 1024 </int>
+				<int> 2048 </int>
+				<real> 80 </real>
 				<vector2> -0.460541, -0.338449 </vector2>
 				<vector2> -0.460541, -27.1862 </vector2>
 				<vector2> 1.35597, -14.9158 </vector2>

scripts/main.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-var time = 0 # The time after we spawned the last spider
+var time = 3 # The time after we spawned the last spider
 var enemy_scn = load("res://scenes/enemy_1.xml") # The spider scene
 var background # The grass node
 var offset = Vector2(0,0)# The offset of the background
+var player # The player
 
 func _ready():
 	randomize() # Randomize the seed for all random functions
-	background = get_node("CanvasLayer/background") # Take the background node
-	
+	background = get_node("background") # Take the background node
+	player = get_node("player")
 	set_fixed_process(true) # We use _fixed_process to spawn spiders
 	set_process(true) # We use _process to move the background
 	
@@ -16,9 +17,10 @@ func _process(delta):
 	offset = -get_viewport().get_canvas_transform().o # The offset of the viewport
 	var o = offset/Vector2(1024,768) # We divide it by the original screen size, so it will match the UV coordinates of the background sprite
 	background.get_material().set_shader_param("Offset",Vector3(o.x,o.y,0)) # Pass the offset to the shader
-	background.set_region_rect(get_viewport_rect()) # And set the region of the background
+	#background.set_region_rect(get_viewport_rect()) # And set the region of the background
 
 func _fixed_process(delta):
+	background.set_pos(player.get_pos())
 	time += delta # Increase the time left till the next spider by the time elapsed
 	if time > 1: # Enough time had passed
 		time = 0 # Reset the timer

scenes/bullet.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<resource_file type="PackedScene" subresource_count="4" version="1.1" version_name="Godot Engine v1.1.rc2.custom_build">
+<resource_file type="PackedScene" subresource_count="4" version="1.1" version_name="Godot Engine v1.1.stable.d3b42e41b0">
 	<ext_resource path="res://scripts/bullet.gd" type="Script"></ext_resource>
 	<ext_resource path="res://gfx/bullet.png" type="Texture"></ext_resource>
 	<resource type="CircleShape2D" path="local://1">
@@ -74,7 +74,7 @@
 			<string> "node_count" </string>
 			<int> 3 </int>
 			<string> "nodes" </string>
-			<int_array  len="163"> 				-1, -1, 1, 0, -1, 37, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 3, 14, 9, 15, 10, 16, 8, 17, 11, 18, 3, 19, 12, 20, 13, 21, 2, 22, 5, 23, 2, 24, 8, 25, 3, 26, 14, 27, 1, 28, 8, 29, 1, 30, 4, 31, 5, 32, 15, 33, 15, 34, 16, 35, 17, 36, 18, 37, 19, 38, 2, 0, 0, 0, 39, 39, -1, 22, 2, 0, 3, 1, 4, 2, 5, 2, 40, 3, 6, 3, 7, 20, 8, 5, 9, 6, 10, 12, 11, 1, 41, 21, 42, 1, 43, 4, 44, 8, 45, 8, 46, 3, 47, 3, 48, 12, 49, 22, 50, 8, 51, 23, 0, 0, 0, 52, 52, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 24, 8, 5, 9, 6, 10, 12, 11, 1, 53, 9, 54, 8, 0 </int_array>
+			<int_array  len="163"> 				-1, -1, 1, 0, -1, 37, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 3, 14, 9, 15, 10, 16, 8, 17, 11, 18, 3, 19, 12, 20, 13, 21, 2, 22, 5, 23, 2, 24, 8, 25, 3, 26, 14, 27, 1, 28, 8, 29, 1, 30, 4, 31, 5, 32, 15, 33, 15, 34, 16, 35, 17, 36, 18, 37, 19, 38, 2, 0, 0, 0, 39, 39, -1, 22, 2, 0, 3, 1, 4, 2, 5, 2, 40, 3, 6, 3, 7, 20, 8, 5, 9, 6, 10, 3, 11, 1, 41, 21, 42, 1, 43, 4, 44, 8, 45, 8, 46, 3, 47, 3, 48, 12, 49, 22, 50, 8, 51, 23, 0, 0, 0, 52, 52, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 24, 8, 5, 9, 6, 10, 12, 11, 1, 53, 9, 54, 8, 0 </int_array>
 			<string> "variants" </string>
 			<array  len="25" shared="false">
 				<node_path> "" </node_path>
@@ -102,7 +102,7 @@
 						<string> "2D" </string>
 						<dictionary  shared="false">
 							<string> "ofs" </string>
-							<vector2> -46.8076, -17.3582 </vector2>
+							<vector2> -57.0116, -21.2897 </vector2>
 							<string> "snap_grid" </string>
 							<bool> False </bool>
 							<string> "snap_offset" </string>
@@ -122,7 +122,7 @@
 							<string> "snap_step" </string>
 							<vector2> 10, 10 </vector2>
 							<string> "zoom" </string>
-							<real> 17.679832 </real>
+							<real> 14.400333 </real>
 						</dictionary>
 						<string> "3D" </string>
 						<dictionary  shared="false">

commit 7ffb3d9d488b35a15c6666dab61882cbd7453d1f

Fix moving background issue
The issue seems to be that the new background node introduced in 3028e1eb
was bogus, at needs to be under the CanvasLayer node. For the lights to work
with CanvasLayer, the "Layer Min" value had to be set to -2 in the player's
Light2D, as in the CanvasLayer.

scenes/game.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<resource_file type="PackedScene" subresource_count="10" version="1.1" version_name="Godot Engine v1.1.stable.d3b42e41b0">
+<resource_file type="PackedScene" subresource_count="10" version="1.1" version_name="Godot Engine v1.1.stable.custom_build">
 	<ext_resource path="res://scripts/main.gd" type="Script"></ext_resource>
 	<ext_resource path="res://gfx/grass.png" type="Texture"></ext_resource>
 	<ext_resource path="res://scenes/death_screen.xml" type="PackedScene"></ext_resource>
@@ -126,12 +126,16 @@
 				<string> "__meta__" </string>
 				<string> "CanvasModulate" </string>
 				<string> "color" </string>
+				<string> "CanvasLayer" </string>
+				<string> "layer" </string>
+				<string> "offset" </string>
+				<string> "rotation" </string>
+				<string> "scale" </string>
 				<string> "background" </string>
 				<string> "Sprite" </string>
 				<string> "material/material" </string>
 				<string> "texture" </string>
 				<string> "centered" </string>
-				<string> "offset" </string>
 				<string> "flip_h" </string>
 				<string> "flip_v" </string>
 				<string> "vframes" </string>
@@ -140,10 +144,6 @@
 				<string> "modulate" </string>
 				<string> "region" </string>
 				<string> "region_rect" </string>
-				<string> "CanvasLayer" </string>
-				<string> "layer" </string>
-				<string> "rotation" </string>
-				<string> "scale" </string>
 				<string> "CanvasLayer 2" </string>
 				<string> "death_screen" </string>
 				<string> "Control" </string>
@@ -185,9 +185,9 @@
 				<string> "player" </string>
 			</string_array>
 			<string> "node_count" </string>
-			<int> 42 </int>
+			<int> 41 </int>
 			<string> "nodes" </string>
-			<int_array  len="804"> 				-1, -1, 1, 0, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 0, 0, 0, 14, 14, -1, 11, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 15, 10, 0, 0, 0, 17, 16, -1, 22, 2, 0, 3, 1, 4, 2, 5, 2, 6, 11, 18, 12, 7, 4, 8, 5, 9, 6, 10, 13, 11, 1, 19, 14, 20, 1, 21, 4, 22, 15, 23, 15, 24, 3, 25, 3, 26, 7, 27, 16, 28, 1, 29, 17, 0, 0, 0, 30, 30, -1, 5, 2, 0, 31, 18, 21, 4, 32, 5, 33, 6, 0, 3, 0, 17, 16, -1, 22, 2, 0, 3, 15, 4, 2, 5, 2, 6, 11, 18, 12, 7, 4, 8, 5, 9, 6, 10, 13, 11, 15, 19, 14, 20, 15, 21, 4, 22, 15, 23, 15, 24, 3, 25, 3, 26, 7, 27, 16, 28, 1, 29, 17, 0, 0, 0, 30, 34, -1, 6, 2, 0, 31, 3, 21, 4, 32, 5, 33, 6, 13, 19, 0, 5, 0, 36, 35, 20, 4, 2, 0, 37, 21, 3, 15, 13, 22, 0, 0, 0, 17, 17, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 6, 10, 7, 11, 1, 19, 24, 20, 1, 21, 4, 22, 15, 23, 15, 24, 3, 25, 3, 26, 7, 27, 16, 28, 15, 29, 25, 0, 0, 0, 17, 38, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 26, 10, 27, 11, 1, 19, 28, 20, 1, 21, 4, 22, 15, 23, 15, 24, 3, 25, 3, 26, 7, 27, 16, 28, 15, 29, 25, 0, 0, 0, 40, 39, 29, 4, 2, 0, 7, 30, 8, 31, 13, 32, 0, 0, 0, 40, 41, 29, 4, 2, 0, 7, 33, 8, 34, 13, 32, 0, 0, 0, 40, 42, 29, 4, 2, 0, 7, 35, 8, 36, 13, 32, 0, 0, 0, 40, 43, 29, 4, 2, 0, 7, 37, 8, 38, 13, 32, 0, 0, 0, 40, 44, 29, 4, 2, 0, 7, 39, 8, 40, 13, 32, 0, 0, 0, 40, 45, 29, 4, 2, 0, 7, 41, 8, 42, 13, 32, 0, 0, 0, 40, 46, 29, 4, 2, 0, 7, 43, 8, 44, 13, 32, 0, 0, 0, 40, 47, 29, 4, 2, 0, 7, 45, 8, 42, 13, 32, 0, 0, 0, 40, 48, 29, 4, 2, 0, 7, 46, 8, 47, 13, 32, 0, 0, 0, 40, 49, 29, 4, 2, 0, 7, 48, 8, 49, 13, 32, 0, 0, 0, 40, 50, 29, 4, 2, 0, 7, 50, 8, 51, 13, 32, 0, 0, 0, 40, 51, 29, 4, 2, 0, 7, 52, 8, 53, 13, 32, 0, 0, 0, 40, 52, 29, 4, 2, 0, 7, 54, 8, 55, 13, 32, 0, 0, 0, 40, 53, 29, 4, 2, 0, 7, 56, 8, 57, 13, 32, 0, 0, 0, 40, 54, 29, 4, 2, 0, 7, 58, 8, 59, 13, 32, 0, 0, 0, 40, 55, 29, 4, 2, 0, 7, 60, 8, 61, 13, 32, 0, 0, 0, 40, 56, 29, 4, 2, 0, 7, 62, 8, 63, 13, 32, 0, 0, 0, 40, 57, 29, 4, 2, 0, 7, 64, 8, 65, 13, 32, 0, 0, 0, 40, 58, 29, 4, 2, 0, 7, 66, 8, 67, 13, 32, 0, 0, 0, 40, 59, 29, 4, 2, 0, 7, 68, 8, 69, 13, 32, 0, 0, 0, 40, 60, 29, 4, 2, 0, 7, 70, 8, 71, 13, 32, 0, 0, 0, 40, 61, 29, 4, 2, 0, 7, 72, 8, 71, 13, 32, 0, 0, 0, 40, 62, 29, 4, 2, 0, 7, 73, 8, 55, 13, 32, 0, 0, 0, 40, 63, 29, 4, 2, 0, 7, 74, 8, 34, 13, 32, 0, 0, 0, 40, 64, 29, 4, 2, 0, 7, 75, 8, 42, 13, 32, 0, 0, 0, 40, 65, 29, 4, 2, 0, 7, 76, 8, 42, 13, 32, 0, 0, 0, 40, 66, 29, 4, 2, 0, 7, 77, 8, 42, 13, 32, 0, 0, 0, 40, 67, 29, 4, 2, 0, 7, 78, 8, 61, 13, 32, 0, 0, 0, 40, 68, 29, 4, 2, 0, 7, 79, 8, 67, 13, 32, 0, 0, 0, 40, 69, 29, 4, 2, 0, 7, 80, 8, 65, 13, 32, 0, 0, 0, 40, 70, 29, 4, 2, 0, 7, 81, 8, 40, 13, 32, 0, 0, 0, 40, 71, 29, 4, 2, 0, 7, 82, 8, 71, 13, 32, 0, 0, 0, 40, 72, 83, 3, 2, 0, 7, 84, 13, 85, 0 </int_array>
+			<int_array  len="753"> 				-1, -1, 1, 0, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 0, 0, 0, 14, 14, -1, 11, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 15, 10, 0, 0, 0, 16, 16, -1, 5, 2, 0, 17, 11, 18, 4, 19, 5, 20, 6, 0, 2, 0, 22, 21, -1, 22, 2, 0, 3, 1, 4, 2, 5, 2, 6, 12, 23, 13, 7, 4, 8, 5, 9, 6, 10, 14, 11, 15, 24, 16, 25, 15, 18, 4, 26, 15, 27, 15, 28, 3, 29, 3, 30, 7, 31, 17, 32, 1, 33, 18, 0, 0, 0, 16, 34, -1, 6, 2, 0, 17, 3, 18, 4, 19, 5, 20, 6, 13, 19, 0, 4, 0, 36, 35, 20, 4, 2, 0, 37, 21, 3, 15, 13, 22, 0, 0, 0, 22, 22, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 6, 10, 7, 11, 1, 24, 24, 25, 1, 18, 4, 26, 15, 27, 15, 28, 3, 29, 3, 30, 7, 31, 17, 32, 15, 33, 25, 0, 0, 0, 22, 38, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 26, 10, 27, 11, 1, 24, 28, 25, 1, 18, 4, 26, 15, 27, 15, 28, 3, 29, 3, 30, 7, 31, 17, 32, 15, 33, 25, 0, 0, 0, 40, 39, 29, 4, 2, 0, 7, 30, 8, 31, 13, 32, 0, 0, 0, 40, 41, 29, 4, 2, 0, 7, 33, 8, 34, 13, 32, 0, 0, 0, 40, 42, 29, 4, 2, 0, 7, 35, 8, 36, 13, 32, 0, 0, 0, 40, 43, 29, 4, 2, 0, 7, 37, 8, 38, 13, 32, 0, 0, 0, 40, 44, 29, 4, 2, 0, 7, 39, 8, 40, 13, 32, 0, 0, 0, 40, 45, 29, 4, 2, 0, 7, 41, 8, 42, 13, 32, 0, 0, 0, 40, 46, 29, 4, 2, 0, 7, 43, 8, 44, 13, 32, 0, 0, 0, 40, 47, 29, 4, 2, 0, 7, 45, 8, 42, 13, 32, 0, 0, 0, 40, 48, 29, 4, 2, 0, 7, 46, 8, 47, 13, 32, 0, 0, 0, 40, 49, 29, 4, 2, 0, 7, 48, 8, 49, 13, 32, 0, 0, 0, 40, 50, 29, 4, 2, 0, 7, 50, 8, 51, 13, 32, 0, 0, 0, 40, 51, 29, 4, 2, 0, 7, 52, 8, 53, 13, 32, 0, 0, 0, 40, 52, 29, 4, 2, 0, 7, 54, 8, 55, 13, 32, 0, 0, 0, 40, 53, 29, 4, 2, 0, 7, 56, 8, 57, 13, 32, 0, 0, 0, 40, 54, 29, 4, 2, 0, 7, 58, 8, 59, 13, 32, 0, 0, 0, 40, 55, 29, 4, 2, 0, 7, 60, 8, 61, 13, 32, 0, 0, 0, 40, 56, 29, 4, 2, 0, 7, 62, 8, 63, 13, 32, 0, 0, 0, 40, 57, 29, 4, 2, 0, 7, 64, 8, 65, 13, 32, 0, 0, 0, 40, 58, 29, 4, 2, 0, 7, 66, 8, 67, 13, 32, 0, 0, 0, 40, 59, 29, 4, 2, 0, 7, 68, 8, 69, 13, 32, 0, 0, 0, 40, 60, 29, 4, 2, 0, 7, 70, 8, 71, 13, 32, 0, 0, 0, 40, 61, 29, 4, 2, 0, 7, 72, 8, 71, 13, 32, 0, 0, 0, 40, 62, 29, 4, 2, 0, 7, 73, 8, 55, 13, 32, 0, 0, 0, 40, 63, 29, 4, 2, 0, 7, 74, 8, 34, 13, 32, 0, 0, 0, 40, 64, 29, 4, 2, 0, 7, 75, 8, 42, 13, 32, 0, 0, 0, 40, 65, 29, 4, 2, 0, 7, 76, 8, 42, 13, 32, 0, 0, 0, 40, 66, 29, 4, 2, 0, 7, 77, 8, 42, 13, 32, 0, 0, 0, 40, 67, 29, 4, 2, 0, 7, 78, 8, 61, 13, 32, 0, 0, 0, 40, 68, 29, 4, 2, 0, 7, 79, 8, 67, 13, 32, 0, 0, 0, 40, 69, 29, 4, 2, 0, 7, 80, 8, 65, 13, 32, 0, 0, 0, 40, 70, 29, 4, 2, 0, 7, 81, 8, 40, 13, 32, 0, 0, 0, 40, 71, 29, 4, 2, 0, 7, 82, 8, 71, 13, 32, 0, 0, 0, 40, 72, 83, 3, 2, 0, 7, 84, 13, 85, 0 </int_array>
 			<string> "variants" </string>
 			<array  len="86" shared="false">
 				<node_path> "" </node_path>
@@ -201,7 +201,7 @@
 				<resource  resource_type="Script" path="res://scripts/main.gd">  </resource>
 				<dictionary  shared="false">
 					<string> "__editor_plugin_screen__" </string>
-					<string> "Script" </string>
+					<string> "2D" </string>
 					<string> "__editor_plugin_states__" </string>
 					<dictionary  shared="false">
 						<string> "2D" </string>
@@ -346,14 +346,14 @@
 					</dictionary>
 				</dictionary>
 				<color> 0.558475, 0.558475, 0.558475, 1 </color>
+				<int> -2 </int>
 				<int> 7175 </int>
 				<resource  resource_type="CanvasItemMaterial" path="local://2">  </resource>
 				<int> -1 </int>
-				<resource  resource_type="Texture" path="res://gfx/grass.png">  </resource>
 				<bool> False </bool>
+				<resource  resource_type="Texture" path="res://gfx/grass.png">  </resource>
 				<color> 1, 1, 1, 1 </color>
 				<rect2> 0, 0, 1024, 768 </rect2>
-				<int> -2 </int>
 				<dictionary  shared="false">
 					<string> "_editor_collapsed" </string>
 					<bool> True </bool>

scenes/player.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<resource_file type="PackedScene" subresource_count="11" version="1.1" version_name="Godot Engine v1.1.stable.d3b42e41b0">
+<resource_file type="PackedScene" subresource_count="11" version="1.1" version_name="Godot Engine v1.1.stable.custom_build">
 	<ext_resource path="res://scripts/player.gd" type="Script"></ext_resource>
 	<ext_resource path="res://gfx/light.png" type="Texture"></ext_resource>
 	<ext_resource path="res://scenes/gun.xml" type="PackedScene"></ext_resource>
@@ -193,9 +193,9 @@
 			<string> "node_count" </string>
 			<int> 13 </int>
 			<string> "nodes" </string>
-			<int_array  len="525"> 				-1, -1, 1, 0, -1, 44, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 14, 10, 15, 11, 16, 8, 17, 12, 18, 13, 19, 8, 20, 3, 21, 3, 22, 9, 23, 2, 24, 2, 25, 5, 26, 2, 27, 8, 28, 7, 29, 7, 30, 8, 31, 8, 32, 1, 33, 4, 34, 5, 35, 14, 36, 15, 37, 16, 38, 17, 39, 18, 40, 19, 41, 2, 42, 20, 43, 8, 44, 21, 45, 22, 0, 0, 0, 46, 46, -1, 27, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 24, 10, 7, 11, 1, 47, 1, 48, 25, 49, 4, 50, 2, 51, 26, 52, 2, 22, 7, 53, 2, 54, 27, 55, 28, 56, 7, 57, 7, 58, 3, 59, 1, 60, 29, 61, 30, 62, 3, 0, 0, 0, 63, 63, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 31, 8, 5, 9, 6, 10, 7, 11, 1, 64, 10, 65, 8, 0, 0, 0, 63, 66, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 32, 8, 5, 9, 6, 10, 7, 11, 1, 64, 12, 65, 8, 0, 0, 0, 68, 67, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 33, 8, 5, 9, 6, 10, 7, 11, 1, 0, 4, 0, 68, 69, 34, 2, 2, 0, 38, 35, 0, 4, 0, 68, 70, 36, 3, 2, 0, 3, 8, 38, 37, 0, 0, 0, 71, 71, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 38, 8, 5, 9, 6, 10, 7, 11, 1, 48, 39, 72, 1, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 8, 80, 41, 0, 0, 0, 81, 81, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 72, 1, 82, 8, 83, 1, 84, 42, 85, 6, 86, 43, 87, 43, 88, 44, 89, 44, 90, 1, 91, 1, 92, 45, 93, 45, 94, 45, 95, 45, 0, 0, 0, 68, 96, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 9, 11, 1, 0, 9, 0, 71, 97, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 46, 8, 5, 9, 6, 10, 7, 11, 1, 48, 47, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 1, 80, 48, 0, 9, 0, 71, 98, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 46, 8, 5, 9, 6, 10, 7, 11, 1, 48, 49, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 40, 79, 1, 80, 48, 0, 0, 0, 99, 99, -1, 9, 2, 0, 100, 3, 101, 5, 102, 50, 103, 51, 104, 1, 105, 2, 106, 52, 107, 53, 0 </int_array>
+			<int_array  len="525"> 				-1, -1, 1, 0, -1, 44, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 12, 8, 13, 9, 14, 10, 15, 11, 16, 8, 17, 12, 18, 13, 19, 8, 20, 3, 21, 3, 22, 9, 23, 2, 24, 2, 25, 5, 26, 2, 27, 8, 28, 7, 29, 7, 30, 8, 31, 8, 32, 1, 33, 4, 34, 5, 35, 14, 36, 15, 37, 16, 38, 17, 39, 18, 40, 19, 41, 2, 42, 20, 43, 8, 44, 21, 45, 22, 0, 0, 0, 46, 46, -1, 27, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 23, 8, 5, 9, 24, 10, 7, 11, 1, 47, 1, 48, 25, 49, 4, 50, 2, 51, 26, 52, 2, 22, 7, 53, 2, 54, 27, 55, 28, 56, 29, 57, 7, 58, 3, 59, 1, 60, 30, 61, 31, 62, 3, 0, 0, 0, 63, 63, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 32, 8, 5, 9, 6, 10, 7, 11, 1, 64, 10, 65, 8, 0, 0, 0, 63, 66, -1, 12, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 33, 8, 5, 9, 6, 10, 7, 11, 1, 64, 12, 65, 8, 0, 0, 0, 68, 67, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 34, 8, 5, 9, 6, 10, 7, 11, 1, 0, 4, 0, 68, 69, 35, 2, 2, 0, 38, 36, 0, 4, 0, 68, 70, 37, 3, 2, 0, 3, 8, 38, 38, 0, 0, 0, 71, 71, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 39, 8, 5, 9, 6, 10, 7, 11, 1, 48, 40, 72, 1, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 41, 79, 8, 80, 42, 0, 0, 0, 81, 81, -1, 25, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 7, 11, 1, 72, 1, 82, 8, 83, 1, 84, 43, 85, 6, 86, 44, 87, 44, 88, 45, 89, 45, 90, 1, 91, 1, 92, 46, 93, 46, 94, 46, 95, 46, 0, 0, 0, 68, 96, -1, 10, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10, 9, 11, 1, 0, 9, 0, 71, 97, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 47, 8, 5, 9, 6, 10, 7, 11, 1, 48, 48, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 41, 79, 1, 80, 49, 0, 9, 0, 71, 98, -1, 21, 2, 0, 3, 1, 4, 2, 5, 2, 6, 3, 7, 47, 8, 5, 9, 6, 10, 7, 11, 1, 48, 50, 72, 8, 49, 4, 73, 8, 74, 8, 75, 3, 76, 3, 77, 7, 78, 41, 79, 1, 80, 49, 0, 0, 0, 99, 99, -1, 9, 2, 0, 100, 3, 101, 5, 102, 51, 103, 52, 104, 1, 105, 2, 106, 53, 107, 54, 0 </int_array>
 			<string> "variants" </string>
-			<array  len="54" shared="false">
+			<array  len="55" shared="false">
 				<node_path> "" </node_path>
 				<bool> True </bool>
 				<real> 1 </real>
@@ -370,6 +370,7 @@
 				<color> 0.681111, 0.681111, 0.681111, 1 </color>
 				<int> -1024 </int>
 				<int> 1024 </int>
+				<int> -2 </int>
 				<int> 2048 </int>
 				<real> 80 </real>
 				<vector2> -0.460541, -0.338449 </vector2>

scripts/main.gd
@@ -4,12 +4,10 @@ var time = 3 # The time after we spawned the last spider
 var enemy_scn = load("res://scenes/enemy_1.xml") # The spider scene
 var background # The grass node
 var offset = Vector2(0,0)# The offset of the background
-var player # The player
 
 func _ready():
 	randomize() # Randomize the seed for all random functions
-	background = get_node("background") # Take the background node
-	player = get_node("player")
+	background = get_node("CanvasLayer/background") # Take the background node
 	set_fixed_process(true) # We use _fixed_process to spawn spiders
 	set_process(true) # We use _process to move the background
 	
@@ -17,10 +15,9 @@ func _process(delta):
 	offset = -get_viewport().get_canvas_transform().o # The offset of the viewport
 	var o = offset/Vector2(1024,768) # We divide it by the original screen size, so it will match the UV coordinates of the background sprite
 	background.get_material().set_shader_param("Offset",Vector3(o.x,o.y,0)) # Pass the offset to the shader
-	#background.set_region_rect(get_viewport_rect()) # And set the region of the background
+	background.set_region_rect(get_viewport_rect()) # And set the region of the background
 
 func _fixed_process(delta):
-	background.set_pos(player.get_pos())
 	time += delta # Increase the time left till the next spider by the time elapsed
 	if time > 1: # Enough time had passed
 		time = 0 # Reset the timer