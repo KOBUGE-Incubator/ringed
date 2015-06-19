b97e91189da9246ebc4358f3c8015e95c9755db0
erik90mx <erik90mx@yahoo.com.mx>
2015-06-19 02:16:21 -0500

    Added a Kamikaze spider that explodes with contact

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -4,22 +4,29 @@ extends "living_object.gd" # Spiders are living objects too...
 export var cooldown = 0.2 # Time between two damages
 export var damage = 1.0 # The amount of damage we do
 var cooldown_left = 0.0 # Time left till the next damage
+var is_attaking = false # To know when the spider is attaking
+var spider_sound # The sounds of the spiders
+var spider_sound_ID # The ID of the spider
 
 func _ready():
 	add_to_group("enemies") # Mark it as an enemy
+	spider_sound = get_node("SpiderSound")
+	spider_sound_ID = spider_sound.play("spider_sound")
 	
 
 func logic(delta): # We override the function defined in moveable_object.gd
 	cooldown_left -= delta # Decrease the cooldown by the time elapsed
-	var spiderSound = get_node("SpiderSound")
-	spiderSound.play("spider_sound")
+
 	target_angle = get_pos().angle_to_point( get_parent().get_node("player").get_pos() ) + deg2rad(0) # Turn towards the player
 	force = Vector2(0,-1).rotated(target_angle) # and move in that same direction
 	
 	for body in get_colliding_bodies(): # Get all colliding bodies
 		if(body.has_method("damage") && cooldown_left <= 0):
 			if(body.damage("spider",damage)): # If we can damage them
+				is_attaking = true # The spider is attaking!
 				cooldown_left = cooldown # Increase the timeout
+		else:
+			is_attaking = false
 
 func amount_of_damage(from): # We override the function defined in living_object.gd
 	if(from != "spider"):
---
+++ scripts/enemy_kamikaze.gd
@@ -0,0 +1,37 @@
+extends "enemy_1.gd" # It has all the variables we need to make damange
+var spider_explote_ID = null # The ID of the actual explote sound
+var it_explotes = false # To know if the Kamikaze explotes
+var spider_animations # The animations of the kamikaze spider
+var health_empty # The health node when its empty
+
+func _ready():
+	spider_animations = get_node("AnimatedSprite/AnimationPlayer")
+	health_empty = get_node("HealthHolder/HealthEmpty")
+	._ready()
+func logic(delta): # We override the function defined in moveable_object.gd
+	.logic(delta) # The enemy_1 method
+	if(is_attaking == true): # The spider are now atacking
+		do_kamikaze()
+	if(it_explotes == true): # The spider explotes 
+		if(!spider_sound.is_voice_active(spider_explote_ID)): # The explotion sound is over... time to die
+			print("audio finalizo")
+			health = -1 # This make the spider die
+	
+func do_kamikaze():
+	spider_sound.stop_voice(spider_sound_ID) # We stop the original spider sound
+	if(spider_explote_ID == null): # If is the first and onlne time that the spider attacks
+		if(!spider_sound.is_voice_active(spider_sound_ID)): # Is the orginial spider sound stoped
+			spider_explote_ID = spider_sound.play("spider_explote") # Reproduces spider explotion sound
+			spider_animations.play("die") # Play de spider dead animation
+			#set_layer_mask(0) # Disable Collisions
+			#set_collision_mask(0) # Disable Collisions
+			disable_spider()
+			it_explotes = true
+func disable_spider():
+	healthbarHolder.hide()
+	healthbar.hide()
+	health_empty.hide()
+	damage = 0
+	speed = 0
+	rotation_speed = 0
+
