c2c6d195bffe1c47ed4945f1cec30e3bad2af742
erik90mx <erik90mx@yahoo.com.mx>
2015-08-22 18:28:17 -0500

    Now the game can be paused when ESC is pressed

---
+++ scripts/pause.gd
@@ -0,0 +1,33 @@
+
+extends PopupPanel
+
+var btn_unpause
+var btn_view_project
+
+func _ready():
+	btn_unpause = get_node("Button")
+	btn_view_project = get_node("Label/Button")
+	btn_view_project.connect("pressed", self, "visit_repo")
+	btn_unpause.connect("pressed", self, "unpause")
+	set_process_input(true)
+
+func _input(event):
+	if(Input.is_action_pressed("ui_cancel")):
+		pause(true)
+
+func unpause():
+	pause(false)
+
+func pause(flag):
+	get_tree().set_pause(flag)
+	if(flag):
+		self.show()
+		Input.set_mouse_mode(0)
+	else:
+		self.hide()
+		Input.set_mouse_mode(1)
+
+func visit_repo():
+	OS.shell_open ("https://github.com/KOBUGE-Games/ringed/issues")
+
+
